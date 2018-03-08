d8d5565ab5a5346b89ff96cdcffc2d329b18650b
Marc <marcgallagher16@gmail.com>
2018-03-08 17:23:26 +0000

    changes

--- public/sketch.js
+++ public/sketch.js
@@ -3,24 +3,7 @@ var socket;
 
 function setup() {
   createCanvas(600, 400);
-
-
   background(0);
-  // Start a socket connection to the server
-  socket = io.connect('http://localhost:3000');
-  // We make a named event called 'mouse' and write an
-  // anonymous callback function
-  socket.on('mouse',
-    // When we receive data
-    function(data) {
-      console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill(0,144,255);
-      noStroke();
-      ellipse(data.x, data.y, 10, 10);
-    }
-  );
-}
 
 function draw() {
   textSize(21);
@@ -41,7 +24,7 @@ function mouseDragged() {
 // Function for sending to the socket
 function sendmouse(xpos, ypos) {
   // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
+  console.log("sendmousedata: " + xpos + " " + ypos);
   
   // Make a little object with  and y
   var data = {
--- server.js
+++ server.js
@@ -1,54 +1,45 @@
-// Based off of Shawn Van Every's Live Web
-// http://itp.nyu.edu/~sve204/liveweb_fall2013/week3.html
-
-// Using express: http://expressjs.com/
+//Require express module from node modules
 var express = require('express');
-// Create the app
-var app = express();
+// Create the canvas app
+var canvas = express();
 
-// Set up the server
-// process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 3000, listen);
+//
+var server = canvas.listen(3000);
 
 // This call back just tells us that the server has started
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+  console.log('Example canvas listening at http://' + host + ':' + port);
 }
+//get all static files from public folder
+canvas.use(express.static('public'));
 
-app.use(express.static('public'));
-
-
-// WebSocket Portion
-// WebSockets work with the HTTP server
+//import socket io library user server as the argument
 var io = require('socket.io')(server);
 
-// Register a callback function to run when we have an individual connection
+// Callback function that initiates for each client that connects
 // This is run for each individual user that connects
 io.sockets.on('connection',
-  // We are given a websocket object in our function
+  // Pass the socket object as a parameter
   function (socket) {
   
-    console.log("We have a new client: " + socket.id);
+    console.log("We have a new artist: " + socket.id);
   
-    // When this user emits, client side: socket.emit('otherevent',some data);
+    // When the user emits data
     socket.on('mouse',
       function(data) {
         // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x + " " + data.y);
+        console.log("Received: 'mouse co-ordinates' " + data.x + " " + data.y);
       
-        // Send it to all other clients
+        // Sends mouse data to all other clients
         socket.broadcast.emit('mouse', data);
-        
-        // This is a way to send to everyone including sender
-        // io.sockets.emit('message', "this goes to everyone");
 
       }
     );
     
     socket.on('disconnect', function() {
-      console.log("Client has disconnected");
+      console.log("Artist has left :(");
     });
   }
 );
